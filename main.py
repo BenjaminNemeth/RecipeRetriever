f5d5c00e6f2463ee2b1df454fe183d91ad4349b6
Benjamin <BenjaminNemeth@outlook.com>
2022-11-05 22:17:38 +0000

    Added svg Map and few pages

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    return render_template("WelcomePage.html")
+
+
+@app.route("/main", methods=["GET", "POST"])
+def mainPage():
     return render_template("main.html")
+
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
+
