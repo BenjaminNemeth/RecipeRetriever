11efafd15d4f39b04c2cf8f8eefe080ffd561276
Benjamin <BenjaminNemeth@outlook.com>
2022-11-27 19:50:50 +0000

    Added some javascript to add some functionality but only to the US and Canada, im currently working on a python script which will configure each country in the svg file to have their own functino, as well as write said function.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import time
+
 from flask import Flask, render_template, url_for, request
+import requests
+import json
+import sys
 
 app = Flask(__name__)
 
@@ -9,10 +14,20 @@ def index():
 
 @app.route("/main", methods=["GET", "POST"])
 def mainPage():
+    if request.method == 'POST':
+        time.sleep(1)
+        return render_template('resultsPage.html')
+
     return render_template("main.html")
 
+
+@app.route('/processUserInput/<string:userData>', methods=['POST'])
+def processInput(userData):
+    userInput = json.loads(userData)
+    print("hi")
+    print(userInput)
+    return render_template("resultsPage.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
-
